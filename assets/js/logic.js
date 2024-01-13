bf0c6eb6c4402e007d2fcaa471097d5504b9a29a
RajpreetKR <rajpreet@khalsa.com>
2024-01-13 13:15:55 +0000

    added varaible i think i will need

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,16 @@
+// variables calling IDs in the index.html
 const startButton = document.querySelector("#start-button");
 const timerDisplay = document.querySelector("#time");
+const startScreen = document.querySelector("#start-screen");
+const questions = document.querySelector("#questions");
+const questionTitle = document.querySelector("#question-title");
+const choices = document.querySelector("#choices");
+const endScreen = document.querySelector("#end-screen");
+const submitButton = document.querySelector("#submit-button");
+const feedback = document.querySelector("#feedback");
 
-let timeLeft = 60; // Total time in seconds
+// variables
+let timeLeft = 60; // total time in seconds
 let timerInterval;
 
 function startTimer() {
@@ -13,9 +22,10 @@ function startTimer() {
     if (timeLeft <= 0) {
         clearInterval(timerInterval);
         // call for a function to end the quiz here when created
+        
     }
 
-  }, 1000); // 1000 milliseconds = 1 second
+    }, 1000); // 1000 milliseconds = 1 second
 }
 
 startButton.addEventListener("click", startTimer);
