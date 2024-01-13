35fd61572fe412075785156ab1592d123ca48e05
RajpreetKR <rajpreet@khalsa.com>
2024-01-13 12:37:51 +0000

    got timer to work when start quiz button is pressed

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,21 @@
-let startBtn = document.querySelector("#start");
-let timer = document.querySelector("#time");
+const startButton = document.querySelector("#start-button");
+const timerDisplay = document.querySelector("#time");
 
+let timeLeft = 60; // Total time in seconds
+let timerInterval;
+
+function startTimer() {
+    timerInterval = setInterval(function() {
+    timeLeft--;
+    timerDisplay.textContent = timeLeft;
+
+    // check if timeLeft is 0, and end the quiz if it is
+    if (timeLeft <= 0) {
+        clearInterval(timerInterval);
+        // call for a function to end the quiz here when created
+    }
+
+  }, 1000); // 1000 milliseconds = 1 second
+}
+
+startButton.addEventListener("click", startTimer);
